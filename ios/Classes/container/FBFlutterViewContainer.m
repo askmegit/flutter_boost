f78b077d844b516fd1082a05ccf8738bcd6823fa
Ives <ives@hellotalk.com>
2023-08-14 17:40:06 +0800

    ios viewWillAppear 导致 flutter didChangeMetrics 后，造成 scrollView FixedScrollMetrics 发生偏移

--- ios/Classes/container/FBFlutterViewContainer.m
+++ ios/Classes/container/FBFlutterViewContainer.m
@@ -274,10 +274,9 @@ _Pragma("clang diagnostic pop")
     //For new page we should attach flutter view in view will appear
     //for better performance.
     [self attatchFlutterEngine];
-    
-    [super bridge_viewWillAppear:animated];
     [self.view setNeedsLayout];//TODO:通过param来设定
-    
+    [self.view layoutIfNeeded];
+    [super bridge_viewWillAppear:animated];
 }
 
 - (void)viewDidAppear:(BOOL)animated
